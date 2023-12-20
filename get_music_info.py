fec997c450f04ee245fdc8ca2300ce550bb0e910
zhoujiahao111 <57947267+zhoujiahao111@users.noreply.github.com>
2023-12-20 16:36:07 +0800

    Update get_music_info.py

--- get_music_info.py
+++ get_music_info.py
@@ -27,62 +27,67 @@ class 网易云类:
         self.等级登录信息 = {
             'params': '',
             "encSecKey": ""}
+
+
         self.榜单数量 = 10
-
         self.会话 = requests.Session()
-        self.账号信息 = {'Content-Type': 'application/x-www-form-urlencoded',
-                         'Cookie': self.主页Cookie, 'Origin': 'https://music.163.com',
-                         'Referer': 'https://music.163.com/user/songs/rank?id=367831881',
-                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29'}
+        self.上一次时间戳, self.上一次剩余听歌量 = self.读取配置文件方法()
+        self.当前时间戳 = self.获取当前时间戳方法()
+        self.当前剩余听歌量 = self.获取当前剩余听歌量方法()
 
-        self.等级账号信息 = {'Content-Type': 'application/x-www-form-urlencoded',
-                             'Cookie': self.等级Cookie, 'Origin': 'https://music.163.com',
-                             'Referer': 'https://music.163.com/user/level',
-                             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29'}
+        if self.上一次时间戳 == '暂无记录':
+            print('暂无上次打开的记录')
+        else:
+            print(f'上次打开的记录: {self.转成格式化时间方法(self.上一次时间戳)} 还需:{self.上一次剩余听歌量}')
 
-        self.获取等级方法()
+        print(self.转成格式化时间方法(self.当前时间戳) + " 距离升级还需听{}首歌".format(self.当前剩余听歌量))
+        self.预测听完天数方法()
         self.获取音乐单方法()
 
-    def 获取等级方法(self):
+    def 获取当前剩余听歌量方法(self) -> int:
         链接 = "https://music.163.com/weapi/user/level"
         会话结果 = self.会话.post(url=链接, data=self.等级登录信息, headers=self.等级账号信息)
 
         等级信息 = json.loads(会话结果.text)['data']
         听歌量 = 等级信息["nextPlayCount"] - 等级信息["nowPlayCount"]
-        print('上次打开的记录: ' + self.读取配置文件方法())
-        print(self.获取当前格式化时间方法() + " 距离升级还需听{}首歌".format(听歌量))
-        self.保存数据至配置文件方法(听歌量)
 
-    def 读取配置文件方法(self):
+        self.保存数据至配置文件方法(听歌量)
+        return 听歌量
+
+    def 读取配置文件方法(self) -> tuple:
         if not path.exists('config.config'):
             文件 = open('config.config', 'w', encoding='utf-8')
             文件.close()
-            return '暂无记录'
+            return '暂无记录', ''
 
         with open('config.config', 'r', encoding='utf-8') as 文件:
             内容 = 文件.read()
             if 内容 != '':
-                return 内容
+                内容 = 内容.split(' ')
+                return int(内容[0]), int(内容[1])
 
-        return '暂无记录'
+        return '暂无记录', ''
 
-    def 保存数据至配置文件方法(self, 听歌量):
+    def 保存数据至配置文件方法(self, 听歌量: int) -> None:
         with open('config.config', 'w', encoding='utf-8') as 文件:
-            文件.write(self.获取当前格式化时间方法() + ' 还需:' + str(听歌量))
+            文件.write(str(self.获取当前时间戳方法()) + ' ' + str(听歌量))
 
-    def 获取当前格式化时间方法(self):
-        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    def 获取当前时间戳方法(self) -> int:
+        return int(datetime.now().timestamp())
 
-    def 获取音乐单方法(self):
+    def 转成格式化时间方法(self, 时间戳: int) -> str:
+        return datetime.fromtimestamp(时间戳).strftime('%Y-%m-%d %H:%M:%S')
+
+    def 获取音乐单方法(self) -> None:
         链接 = "https://music.163.com/weapi/v1/play/record"
         会话结果 = self.会话.post(url=链接, data=self.登录信息, headers=self.账号信息)
 
         音乐单信息 = json.loads(会话结果.text)
 
-        self.获取听歌榜单方法(音乐单信息['weekData'], '每周')
-        self.获取听歌榜单方法(音乐单信息['allData'], '全部')
+        self.输出听歌榜单方法(音乐单信息['weekData'], '每周')
+        self.输出听歌榜单方法(音乐单信息['allData'], '全部')
 
-    def 获取听歌榜单方法(self, 数据: list, 类型):
+    def 输出听歌榜单方法(self, 数据: list, 类型: str, 数量=10) -> None:
         if self.榜单数量 > len(数据):
             self.榜单数量 = len(数据)
 
@@ -91,5 +96,13 @@ class 网易云类:
             音乐信息 = 数据[i]
             print('第{}名: {}  听歌次数:{}次'.format(str(i + 1), 音乐信息['song']['name'], 音乐信息['playCount']))
 
+    def 预测听完天数方法(self) -> None:
+        try:
+            每天减少的听歌量 = 86400 / (
+                    (self.当前时间戳 - self.上一次时间戳) / (self.上一次剩余听歌量 - self.当前剩余听歌量))
+            每天减少的听歌量 = 每天减少的听歌量 if 每天减少的听歌量 <= 300 else 300
+            print(f'预计还需{int(self.当前剩余听歌量 / 每天减少的听歌量)}天听完')
+        except:
+            print('间隔时间过小，无法预测剩余听完的时间')
 
 网易云类()
